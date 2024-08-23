5976ad38df0adda8de1603bab4d104410ca3c933
nicolasdp8 <nicolas00144@gmail.com>
2024-08-22 23:07:50 -0300

    cambios

---
+++ numeros_pares.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display errors', '1');
+ini_set('display_startup_errors', '1');
+
+error_reporting(E_ALL);
+
+for ($i = 0; $i <= 100; $i += 2) {
+    echo $i . "<br>";
+    if ($i == 60) {
+        break;
+    }
+}
